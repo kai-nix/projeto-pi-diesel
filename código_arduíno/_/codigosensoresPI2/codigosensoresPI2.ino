ea18270fa6ea2ef8b269a6023e5275a7dca0265a
GiovanaZukauskas <giovana.ratao@sptech.school>
2024-09-10 13:02:51 -0300

    Subindo código Arduíno Final

--- código_arduíno/_/codigosensoresPI2/codigosensoresPI2.ino
+++ código_arduíno/_/codigosensoresPI2/codigosensoresPI2.ino
@@ -12,29 +12,38 @@ const int PINO_SENSOR_TEMPERATURA = A0;
 
 
 float temperaturaCelsius;
+float tempMax;
+float tempMin;
 
 void setup() {
     Serial.begin(9600);
 }
 
-void loop() {
+void loop(){
 
 long distancia = sensor.distance();
 
 int valorLeitura = analogRead(PINO_SENSOR_TEMPERATURA);
 temperaturaCelsius = (valorLeitura * 5.0 / 1023.0) / 0.01;
 
-	Serial.print("Distância: ");
+	Serial.print("Distância:");
 	Serial.print(distancia);
-	Serial.println(" cm");
-	
+	Serial.print(" ");
+  Serial.println();
 
-	Serial.print("Temperatura: ");
+  
+  Serial.print("Temperatura:");
 	Serial.print(temperaturaCelsius);
-	Serial.println(" C");
+  Serial.print(" ");
+  
 
+Serial.print("tempMax:");
+    Serial.print(52);
+    Serial.print(" ");
 
-
+    Serial.print("tempMin:");
+    Serial.print(21);
+    Serial.println();
 
 delay(2000);
 
